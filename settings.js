d74f9377dcd3773414b22be4f6efe552e2530a4e
tcely <tcely@users.noreply.github.com>
2018-01-19 12:07:26 -0500

    Update settings when closing settings page.

--- settings.js
+++ settings.js
@@ -1,3 +1,16 @@
 window.addEvent("domready", function () {
     new FancySettings.initWithManifest();
 });
+
+// Update the settings
+window.addEvent("close", function() {
+  var settings = new Store('settings', {
+	  "rpcpath" : "http://localhost:6800/jsonrpc",
+	  "rpcuser" : "",
+	  "rpctoken" : "",
+      });
+
+  chrome.storage.local.set({"rpcpath":settings.get('rpcpath')});
+  chrome.storage.local.set({"rpcuser":settings.get('rpcuser')});
+  chrome.storage.local.set({"rpctoken":settings.get('rpctoken')});
+});
